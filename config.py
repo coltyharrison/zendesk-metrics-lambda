84c23a96215060a1e82801029f9f53fed66ad6ac
Colty Harrison <coltyharrison@gmail.com>
2018-07-11 23:58:35 -0700

    Add fake config.py file for reference

---
+++ config.py
@@ -0,0 +1,9 @@
+# This is a sample config file. Replace the values with your credentials
+# to get it to function properly :)
+
+
+class Config(object):
+    ZENDESK_TOKEN = b'fakeemailn@gmail.com/token:jksjfaJHsdfjh239JKhdsfkjhJHd9f9uskhsdf'
+    ZENDESK_URL = 'https://fakesubdomain.zendesk.com'
+    ZENDESK_VIEW = '358102938402'
+    S3_BUCKET_NAME = 'this-is-your-s3-bucket-name'
